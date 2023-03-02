dd00a877a322048053948acc839cb3d4cf5da83d
Greg Randall <gregr@G-MacBook-Pro.local>
2023-03-02 09:51:08 -0500

    removed commented out code

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-date_default_timezone_set(get_timezone()); // change this to your local country time zone
+date_default_timezone_set(get_timezone()); 
 include 'GIFEncoder.class.php';
 
 if(empty($_GET["dt"])){
@@ -9,23 +9,6 @@ if(empty($_GET["dt"])){
 $time = str_replace("/", " ", $_GET["dt"]);
 
 
-//if(empty($_GET["f"])){
-//    die("format must be set... ?f=a-H-I-S  ");
-//}
-//$ff = explode("-", $_GET["f"]);
-//$f = "";
-//for($i=0; $i<count($ff); $i++){
-//    $ff[$i] = "%".$ff[$i];
-////    $f = $f."%".$frf[$i]."";
-//    //%a:%H:%I:%S
-//}
-//$f = implode(":", $ff);
-//
-//
-//if(empty($_GET["t"])){
-//    die("transition must be set... ?t=second  .... second or minute or hour");
-//}
-//$t = $_GET["t"];
 
 $future_date = new DateTime(date('r',strtotime($time)));
 $time_now = time();
@@ -36,23 +19,7 @@ $t = "second";
 
 $frames = array();
 $delays = array();
-//$is_cache = true;
-//if(!empty($_GET["cache"])){
-//    if($_GET["cache"]==0){
-//        $is_cache = false;
-//    }
-//}
 $cache_file = "cache/".preg_replace("/[^a-z,A-Z,0-9,_]/", "_", $time).".gif";
-//if(file_exists($cache_file) & $is_cache){
-//    header( 'Expires: Sun, 03 Mar 2015 16:25:00 GMT' ); //expire this image instantly
-//    header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' );
-//    header( 'Cache-Control: no-store, no-cache, must-revalidate' );
-//    header( 'Cache-Control: post-check=0, pre-check=0', false );
-//    header( 'Pragma: no-cache' );
-//    header('Content-type:image/gif');
-//    echo file_get_contents($cache_file);
-//    die();
-//}
 
 
 $image = imagecreatefrompng('background/1.png'); // change background 
@@ -100,9 +67,6 @@ for($i = 0; $i <= 60; $i++){
 }
 
 $gif = new AnimatedGif($frames,$delays,$loops);
-//if($is_cache){
-    file_put_contents($cache_file, $gif->getAnimation());
-//}
 header( 'Expires: '.gmdate('D, d M Y H:i:s T', strtotime($time)) ); //expire this image
 header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' );
 header( 'Cache-Control: no-store, no-cache, must-revalidate' );
@@ -118,7 +82,7 @@ function get_timezone(){
     return ($geolocation['geoplugin_timezone']);
 }
 
-//https://stackoverflow.com/a/13646735/4896819
+ //https://stackoverflow.com/a/13646735/4896819
 function getUserIP( ) {
     if ( isset( $_SERVER[ "HTTP_CF_CONNECTING_IP" ] ) ) {
         $_SERVER[ 'REMOTE_ADDR' ]    = $_SERVER[ "HTTP_CF_CONNECTING_IP" ];
